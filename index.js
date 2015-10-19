d53d486b3f6e97063052e596a0b0a1607f402da7
Jenny Pollack <jpollack@ornicept.com>
2015-10-19 11:14:24 -0700

    remove cool

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cool = require('cool-ascii-faces');
+
 var express = require('express');
 var app = express();
 
@@ -14,9 +14,9 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
